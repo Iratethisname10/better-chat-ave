a349009a85e075b225a64d881c7c9389dd7cbb0c
IRateThisName10 <115908538+Iratethisname10@users.noreply.github.com>
2024-09-04 17:41:58 +0800

    Create inject.js

---
+++ src/inject.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Better Chat Ave
+// @version      3/9/2024
+// @description  Keybinds and other stuff for chat ave
+// @author       Vocat
+// @match        *://chat-avenue.com/teen*
+// @require      https://raw.githubusercontent.com/Iratethisname10/better-chat-ave/main/main.js
+// @run-at       start
+// ==/UserScript==
