663018d17c831ebec431b691d023caf236b90a6e
IRateThisName10 <115908538+Iratethisname10@users.noreply.github.com>
2024-09-05 10:59:36 +0800

    Add files via upload

---
+++ src/inject.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Better Chat Ave
+// @version      3/9/2024
+// @description  Keybinds and other stuff for chat ave
+// @author       Vocat
+// @match        https://www.chat-avenue.com/teen/
+// @require      https://raw.githubusercontent.com/Iratethisname10/better-chat-ave/main/src/main.js
+// @run-at       start
+// ==/UserScript==
