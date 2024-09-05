3e57a1d5dd8f826cee7aefe11b7f9b1e828adc8c
IRateThisName10 <115908538+Iratethisname10@users.noreply.github.com>
2024-09-05 10:57:09 +0800

    Delete src/inject.js

--- src/inject.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name         Better Chat Ave
-// @version      3/9/2024
-// @description  Keybinds and other stuff for chat ave
-// @author       Vocat
-// @match        *://chat-avenue.com/teen*
-// @require      https://raw.githubusercontent.com/Iratethisname10/better-chat-ave/main/src/main.js
-// @run-at       start
-// ==/UserScript==
