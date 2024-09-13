e28519b22961c7abc8b293b1c12d97b2dc922fb8
IRateThisName10 <115908538+Iratethisname10@users.noreply.github.com>
2024-09-13 09:22:35 +0800

    actually auto updates

--- src/inject.js
+++ src/inject.js
@@ -4,8 +4,17 @@
 // @description  Keybinds and other stuff for chat ave
 // @author       Vocat
 // @match        https://www.chat-avenue.com/teen/
-// @require      https://raw.githubusercontent.com/Iratethisname10/better-chat-ave/main/src/main.js
 // @run-at       start
 // ==/UserScript==
 
-// the actuall script is loaded through @require
+(() => {
+	'use strict';
+
+	fetch('https://raw.githubusercontent.com/Iratethisname10/better-chat-ave/main/src/main.js')
+		.then(response => {
+			if (!response.ok) throw new Error(`http error, status: ${response.status}`);
+			return response.text();
+		})
+		.then(script => new Function(script)())
+		.catch(err => console.error('an error occurred while executing'));
+})();
